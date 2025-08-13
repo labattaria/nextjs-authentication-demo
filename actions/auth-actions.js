aa35a0dbab40b0f66269330abe3583cd3d96757e
labattaria <hallucinate322@gmail.com>
2025-08-13 04:02:53 +0300

    add server action for user signup; add basic validation

---
+++ actions/auth-actions.js
@@ -0,0 +1,28 @@
+"use server";
+
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters long.";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+
+  const hashedPassword = hashUserPassword(password);
+  createUser(email, hashedPassword);
+}
